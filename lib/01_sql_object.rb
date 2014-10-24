c2d5226cb4a7dd7ce08414b1541f24b826912c69
Isis Anchalee <isiswenger@gmail.com>
2014-10-24 11:49:55 -0700

    Getter and Setters Added

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -2,21 +2,37 @@ require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
-
+ # bundle exec rspec spec/01_sql_object_spec.rb
 class SQLObject
   def self.columns
-    # ...
+
+    query = <<-SQL
+    SELECT *
+    FROM "#{table_name}"
+    SQL
+
+    tables = DBConnection.execute2(query)
+    @columns = tables.first.map(&:to_sym)
   end
 
   def self.finalize!
+    self.columns.each do |col|
+      define_method(col) do
+        self.attributes[col]
+      end
+
+      define_method("#{col}=") do |value|
+        self.attributes[col] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.name.underscore.pluralize
   end
 
   def self.all
@@ -36,7 +52,7 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
